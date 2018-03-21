7df89d7b9ed227bbf60da4bd45c93a27e0fcf70b
Omar Ihmoda <omarihmoda@DN51v5rr.SUNet>
2018-03-21 07:03:01 -0700

    added loading bar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import New from "./New";
 import Poll from "./Poll";
+import LoadingBar from "react-redux-loading";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={New} />
-          <Route exact path="/:id" component={Poll} />
-        </Switch>
-      </BrowserRouter>
+      <div>
+        <LoadingBar />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={New} />
+            <Route exact path="/:id" component={Poll} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import { loadingBarReducer } from "react-redux-loading";
 
 import criteria from "./criteria";
 import categories from "./categories";
 
 export default combineReducers({
   criteria,
-  categories
+  categories,
+  loadingBar: loadingBarReducer
 });
