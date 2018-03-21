176953e32cf7b07e8c1b0b0dc3d65387306bd476
Omar Ihmoda <omarihmoda@DN51v5rr.SUNet>
2018-03-20 17:58:59 -0700

    initial commit

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -2,13 +2,35 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Result extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  handleOnClick = (event, category) => {
+    const selected = this.state[category] ? false : true;
+    console.log(selected);
+    this.setState(() => ({
+      [category]: selected
+    }));
+  };
+
   render() {
     const { categories } = this.props;
     return (
       <div className="category-container">
         {categories.map(category => {
           return (
-            <div className="category" key={category.category}>
+            <div
+              className="category"
+              style={{
+                outline: this.state[category.category]
+                  ? "4px solid #FCDDA5"
+                  : "none"
+              }}
+              key={category.category}
+              data-id={category.category}
+              onClick={e => this.handleOnClick(e, category.category)}
+            >
               <p className="category-number">{category.number}</p>
               <p className="category-text">{category.category}</p>
             </div>
