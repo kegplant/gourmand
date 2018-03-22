8aa211409cf98f2e45ad8866d8f852a336d9c31c
kegplant <ace1905@gmail.com>
2018-03-22 15:47:22 -0700

    add polls controller: create and get by _id

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,31 +7,34 @@
 // Require controller.js file and set it to a variable:
 // ( Change the "controller" variable name and the controller file name
 // within the 'controllers' directory. )
-var controller = require("../controllers/controller_name_plural.js");
-var yelpQueries = require("../controllers/yelpQueries.js");
+const controller = require("../controllers/controller_name_plural.js");
+const yelpQueries = require("../controllers/yelpQueries.js");
+const polls = require("../controllers/polls.js");
+
 const dummy_data = require("./dummy_data");
 // Export all routes to server.js:
-module.exports = function(app) {
-  // Root route - renders index.ejs view (for socket.io example):
-  app.get("/", function(request, response) {
-    response.render("index");
-  });
+module.exports = function (app) {
+    // Root route - renders index.ejs view (for socket.io example):
+    app.get("/", function (request, response) {
+        response.render("index");
+    });
 
-  // Another example route - responses with JSON object:
-  app.get("/users", function(request, response) {
-    controller.getControllerMethod(request, response); //<-- CHANGE "controller" variable name(2)
-  }); // <-- DO NOT ADD COMMAS if ADDING ROUTES BELOW.
-  app.post("/yelp/categories", function(request, response) {
-    console.log("in server");
-    yelpQueries.categories(request, response);
-  });
+    // Another example route - responses with JSON object:
+    app.get("/users", function (request, response) {
+        controller.getControllerMethod(request, response); //<-- CHANGE "controller" variable name(2)
+    }); // <-- DO NOT ADD COMMAS if ADDING ROUTES BELOW.
+    app.post("/yelp/categories", function (request, response) {
+        console.log("in server");
+        yelpQueries.categories(request, response);
+    });
 
-  app.post("/polls/new", function(request, response) {
-    response.json({ response: request.body });
-  });
+    app.post("/polls/new", function (request, response) {
+        polls.create(request, response);
+    });
 
-  app.get("/polls/:id", function(request, response) {
-    console.log(request.params.id);
-    response.json(dummy_data);
-  });
+    app.get("/polls/:id", function (request, response) {
+        // console.log(request.params.id);
+        // response.json(dummy_data);
+        polls.get(request, response);
+    });
 };
--- server/controllers/yelpQueries.js
+++ server/controllers/yelpQueries.js
@@ -9,45 +9,57 @@ const price = ["1, 2, 3, 4", "1", "2", "3", "4"];
 module.exports = {
     categories: function (req, res) {
         console.log(req.body);
-        const query = this.formatQuery(req.body);
+        const query = formatQuery(req.body);
         client
             .search(query)
             .then(response => {
-                res.json(this.categoryBuilder(response.jsonBody.businesses));
+                res.json(categoryBuilder(response.jsonBody.businesses));
             })
             .catch(e => {
                 console.log(e);
             });
     },
-    formatQuery: function (query) { //result sorted by "best match" at the moment
-        //code to format the query
-        return {
-            term: meal[query.meal - 1], //correct
-            price: price[query.price - 1], //correct
-            location: query.address, //takes zipcode as well
-            radius: query.location * 1600, //correct
-            limit: 50 //maximum per query; can use offect:50 to get 51-100 etc.
-        };
-    },
-    categoryBuilder: function (businesses) {
-        let categories = {};
-        businesses.forEach(business => {
-            business.categories.forEach(category => {
-                categories[category.title] = categories[category.title] ? categories[category.title] + 1 : 1;
+    getRecommendations: function (req, res) {
+        console.log(req.body);
+        const query = formatQuery(req.body);
+        client
+            .search(query)
+            .then(response => {
+                res.json(categoryBuilder(response.jsonBody.businesses));
             })
-        })
-        // categories=Object.entries(categories);
-        let categoryList = []
-        for (category in categories) {
-            let entry = {
-                category: category,
-                number: categories[category],
-                image: "static/images/chinese.png"
-            }
-            categoryList.push(entry); //could use some optimization
-        }
-        categoryList.sort((a, b) => b.number - a.number);
-        console.log(categoryList)
-        return categoryList;
-    },
+            .catch(e => {
+                console.log(e);
+            });
+    }
 };
+formatQuery = function (query) { //result sorted by "best match" at the moment
+    //code to format the query
+    return {
+        term: meal[query.meal - 1], //correct
+        price: price[query.price - 1], //correct
+        location: query.address, //takes zipcode as well
+        radius: query.location * 1600, //correct
+        limit: 50 //maximum per query; can use offect:50 to get 51-100 etc.
+    };
+};
+categoryBuilder = function (businesses) {
+    let categories = {};
+    businesses.forEach(business => {
+        business.categories.forEach(category => {
+            categories[category.title] = categories[category.title] ? categories[category.title] + 1 : 1;
+        })
+    })
+    // categories=Object.entries(categories);
+    let categoryList = []
+    for (category in categories) {
+        let entry = {
+            category: category,
+            number: categories[category],
+            image: "static/images/chinese.png"
+        }
+        categoryList.push(entry); //could use some optimization
+    }
+    categoryList.sort((a, b) => b.number - a.number);
+    console.log(categoryList)
+    return categoryList;
+}
