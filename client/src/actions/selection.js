7b605af2b9bde3e9658ba11067c0c1fdeaaf086e
Omar Ihmoda <omarihmoda@DN51v5rr.SUNet>
2018-03-23 16:19:03 -0700

    fixed flickering issue

--- client/src/actions/selection.js
+++ client/src/actions/selection.js
@@ -3,23 +3,12 @@ export const ADD_SELECTION = "ADD_SELECTION";
 export const ADD_ALL_SELECTIONS = "ADD_ALL_SELECTIONS";
 export const REMOVE_SELECTION = "REMOVE_SELECTION";
 export const REMOVE_ALL_SELECTIONS = "REMOVE_ALL_SELECTIONS";
-export const ADD_VOTE = "ADD_VOTE";
 
 export function addSelection(selection) {
   return {
     type: ADD_SELECTION,
     selected: {
       ...selection
-    }
-  };
-}
-
-export function addVote(votes, data) {
-  return {
-    type: ADD_VOTE,
-    votes,
-    data: {
-      ...data
     }
   };
 }
@@ -48,11 +37,6 @@ export function removeAllSelections() {
 
 export function handleAddVote(data, cb) {
   return (dispatch, getState) => {
-    const state = getState();
-    const { votes } = state.selection[data.selected];
-    votes.voters.push(data.id);
-    votes.number = votes.voters.length;
-    dispatch(addVote(votes, data));
     _addVote(data).then(result => {
       cb();
     });
