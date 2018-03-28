bceec4012f6b53af2ade858658c80218e23fd15b
Omar Ihmoda <omarihmoda@Omars-MacBook-Pro.local>
2018-03-27 19:38:55 -1000

    improved rec ui

--- client/src/components/Recommendations.js
+++ client/src/components/Recommendations.js
@@ -4,6 +4,17 @@ import FaStar from "react-icons/lib/fa/star";
 import FaStarHalfEmpty from "react-icons/lib/fa/star-half-empty";
 
 class Recommendations extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: ""
+    };
+  }
+
+  handleRecClicked = (event, id) => {
+    console.log(id);
+  };
+
   getStarsArray = recommendations => {
     const result = recommendations.map(restaurant => {
       const num = Math.floor(restaurant.rating);
@@ -25,11 +36,15 @@ class Recommendations extends Component {
     const { recommendations } = this.props;
     const starsArray = this.getStarsArray(recommendations);
     return (
-      <div>
+      <div className="rec-container">
         <h5>Your group's recommendations</h5>
         {recommendations.map((rec, index) => {
           return (
-            <div className="recommendation" key={rec.id}>
+            <div
+              className="recommendation"
+              key={rec.id}
+              onClick={e => this.handleRecClicked(e, index)}
+            >
               <div className="row rec-title">
                 <p className="rec-name">{rec.name} </p>
                 <div className="row stars-container">
@@ -52,6 +67,7 @@ class Recommendations extends Component {
             </div>
           );
         })}
+        <button className="btn btn-success">Share Selection</button>
       </div>
     );
   }
